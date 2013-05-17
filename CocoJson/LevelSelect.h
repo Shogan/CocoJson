6895fb475e1bc49138aa690afc23337615a003a5
Sean Duffy <sean.duffy@xtravirt.com>
2013-05-17 16:01:15 +0100

    Uploading initial POC project
    Initial POC demonstrating fetching of JSON data and use of this to
    generate a browsable cocos2d menu showcasing apps/games.

---
+++ CocoJson/LevelSelect.h
@@ -0,0 +1,22 @@
+#import "cocos2d.h"
+#import "HelloWorldLayer.h"
+
+@interface LevelSelect : CCScene
+{
+	LevelSelect *_layer;
+}
+
+@end
+
+@interface LevelSelectLayer : CCLayer 
+{
+    NSMutableData *_responseData;
+    UIActivityIndicatorView *_spinner;
+}
+
+@property (nonatomic, retain) NSMutableData *responseData;
+
+-(void)back: (id)sender;
+-(void)openApp: (id)sender;
+
+@end
---
+++ CocoJson/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocoJson
+//
+//  Created by Xtravirt Administrator on 17/05/2013.
+//  Copyright Sean Duffy 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
